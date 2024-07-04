11029b3ccadb78d3ef010ebfabf117adb6af2160
Galers <galosaandrew@gmail.com>
2024-07-04 21:59:10 +0300

    make build

---
+++ index.c8611bcc.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");t.style.top="".concat(50,"%"),t.style.left="".concat(50,"%"),t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.c8611bcc.js.map
